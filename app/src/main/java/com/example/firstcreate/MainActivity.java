f9e30604679faa61f8bc58c2f639e5dedd646c7f
RyuseiInoue <2020m033@kuas.ac.jp>
2020-06-05 09:25:12 +0900

    I made 3 Activities and laid out MainActivity. It might be the second time.

--- app/src/main/java/com/example/firstcreate/MainActivity.java
+++ app/src/main/java/com/example/firstcreate/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.firstcreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogActivity(View view) {
+        Intent toLog = new Intent(this, LogActivity.class);
+        startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this, MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void goToHandleActivity(View view) {
+        Intent toHandle = new Intent(this, HandleActivity.class);
+        startActivity(toHandle);
     }
 }
