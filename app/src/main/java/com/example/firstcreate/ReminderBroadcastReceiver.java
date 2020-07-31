754582030d38b060fc31b0978cb429e7f9f0bae0
RyuseiInoue <2020m033@kuas.ac.jp>
2020-07-31 22:19:56 +0900

    Homework 5.2: extracted string resources in activity_main.xml

---
+++ app/src/main/java/com/example/firstcreate/ReminderBroadcastReceiver.java
@@ -0,0 +1,29 @@
+package com.example.firstcreate;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class ReminderBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        /////throw new UnsupportedOperationException("Not yet implemented");
+
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(context, "BT_Tracker_Channel")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Notification from BT Tracker")
+                .setContentText("Please log your body temperature now")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(context);
+        notificationManager.notify(200, builder.build());
+    }
+}
