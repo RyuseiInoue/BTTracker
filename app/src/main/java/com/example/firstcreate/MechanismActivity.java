53fd34fafb0a77ab65768723f89ead86d6ba7816
RyuseiInoue <2020m033@kuas.ac.jp>
2020-07-31 23:51:42 +0900

    add a new link & implementing onClickListener for Mechanism Activity

--- app/src/main/java/com/example/firstcreate/MechanismActivity.java
+++ app/src/main/java/com/example/firstcreate/MechanismActivity.java
@@ -2,13 +2,34 @@ package com.example.firstcreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MechanismActivity extends AppCompatActivity {
+public class MechanismActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mechanism);
+
+        TextView WhyDoWeHaveFever = findViewById(R.id.WhyDoWeHaveFever);
+        WhyDoWeHaveFever.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.WhyDoWeHaveFever:
+                Intent toLink3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scientificamerican.com/article/what-causes-a-fever/"));
+                startActivity(toLink3);
+                break;
+        }
+    }
+
 }
