f0fa131584fea6532b7f4278bb86f63bf5c98f3c
RyuseiInoue <2020m033@kuas.ac.jp>
2020-07-31 19:19:27 +0900

    Homework 7.1: implemented ScrollView in NormalActivity

--- app/src/main/java/com/example/firstcreate/NormalActivity.java
+++ app/src/main/java/com/example/firstcreate/NormalActivity.java
@@ -2,7 +2,9 @@ package com.example.firstcreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NormalActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+    }
+
+    public void normalToHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
