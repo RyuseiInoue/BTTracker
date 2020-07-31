9c42671f5ee8dc76a46218a90270b74f93d832cd
RyuseiInoue <2020m033@kuas.ac.jp>
2020-07-31 23:41:46 +0900

    implementing onClickListener for Handle Activity

--- app/src/main/java/com/example/firstcreate/HandleActivity.java
+++ app/src/main/java/com/example/firstcreate/HandleActivity.java
@@ -6,18 +6,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class HandleActivity extends AppCompatActivity {
+public class HandleActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handle);
+
+        TextView ClickHere = findViewById(R.id.ClickHere);
+        ClickHere.setOnClickListener(this);
     }
 
-    public void goToLink2(View view) {
-        Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
-
-        startActivity(toLink2);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ClickHere:
+                Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                startActivity(toLink2);
+                break;
+        }
     }
+
 }
